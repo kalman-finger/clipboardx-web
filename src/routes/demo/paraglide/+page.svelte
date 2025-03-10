8ee6651a69c41f467e3b744e2086561e50b77ab1
Mikhail Borisov <mikhailborisov1993@gmail.com>
2025-03-10 17:07:54 +0300

    Initial commit

---
+++ src/routes/demo/paraglide/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
+	import { i18n } from '$lib/i18n';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+	import * as m from '$lib/paraglide/messages.js';
+
+	function switchToLanguage(newLanguage: AvailableLanguageTag) {
+		const canonicalPath = i18n.route(page.url.pathname);
+		const localizedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+		goto(localizedPath);
+	}
+</script>
+
+<h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
+<div>
+	<button onclick={() => switchToLanguage('en')}>en</button>
+	<button onclick={() => switchToLanguage('ru')}>ru</button>
+	<button onclick={() => switchToLanguage('de')}>de</button>
+	<button onclick={() => switchToLanguage('es')}>es</button>
+	<button onclick={() => switchToLanguage('zh')}>zh</button>
+	<button onclick={() => switchToLanguage('ja')}>ja</button>
+</div>
